1c7fa154f2c963af5ee7920794210cd6e58def5c
Ed Morris <102766519+datawithed@users.noreply.github.com>
2022-09-19 10:05:18 +0100

    Update 📁_Portfolio.py

--- pages/📁_Portfolio.py
+++ pages/📁_Portfolio.py
@@ -16,10 +16,15 @@ st.write("""
 st.markdown("""
             ### Tennis analysis:
             - Australian Open 2019 Final: https://datawithed-ao-2019-ao-2019-final-dashboard-qfhq5f.streamlitapp.com/
+            - Queen's Club Championships 2019 (Doubles) Final - Coming Soon!
+            - Self-Improvement Tennis Analytics using SwingVision - Coming Soon!
             ### Football analysis:
             - Serie A (Wyscout event data): https://datawithed-serie-a-multipage-dash-intro-4yxstz.streamlitapp.com/
             ### Medium Articles:
             - https://medium.com/@datawithed
+            ### Club Performance Review
+            - Coming Soon!
+            ### 
             """)
             
 st.markdown("""
